6c795795ac00a962ecd1e75c093e556819dade6c
GaryWorthingtonUK <garyworthingtonuk@googlemail.com>
2025-02-25 20:55:57 +0000

    Updated exif android implementation to remove PluginRegistry

--- android/src/main/kotlin/com/beakyn/exif/ExifPlugin.kt
+++ android/src/main/kotlin/com/beakyn/exif/ExifPlugin.kt
@@ -1,44 +1,19 @@
 package com.beakyn.exif
 
-import kotlin.math.*
 
-import androidx.annotation.NonNull;
-import android.media.ExifInterface;
+import android.media.ExifInterface
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
-//import io.flutter.plugin.common.PluginRegistry.Registrar
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-
-
 import java.io.IOException
+import kotlin.math.*
 
 /** ExifPlugin */
 public class ExifPlugin: FlutterPlugin, MethodCallHandler {
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "beakyn.com/exif")
-    channel.setMethodCallHandler(ExifPlugin());
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-//  companion object {
-//    @JvmStatic
-//    fun registerWith(registrar: Registrar) {
-//      val channel = MethodChannel(registrar.messenger(), "beakyn.com/exif")
-//      channel.setMethodCallHandler(ExifPlugin())
-//    }
-//  }
+  lateinit var channel: MethodChannel;
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "beakyn.com/exif")
@@ -112,9 +87,6 @@ public class ExifPlugin: FlutterPlugin, MethodCallHandler {
     val second = (coordinate3 * 1000.0).toInt()
 
     return "$degree/1,$minute/1,$second/1000"
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
   }
 
   val tags = arrayOf(
